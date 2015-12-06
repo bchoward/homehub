a04588c2f9db3c1eab4fbb9752a4ea955bcd5413
bchoward <bchoward@gmail.com>
2015-12-05 20:53:58 -0800

    xx

--- homehub/homehub/config.py
+++ homehub/homehub/config.py
@@ -1,10 +1,16 @@
-import os
+from os.path import (
+    join as ospjoin,
+    dirname,
+    abspath
+)
 from ConfigParser import ConfigParser
 
 
-_basedir = os.path.abspath(os.path.dirname(__file__))
+_basedir = abspath(dirname(__file__))
+
 
 class BaseConfiguration(object):
     conf = ConfigParser()
-    conf.read('homehub/config.ini')
+    #conf.read('homehub/config.ini')
+    conf.read(ospjoin(_basedir,'homehub','config.ini'))
     DB_URI= conf.get('homehub', 'DB_URI')
