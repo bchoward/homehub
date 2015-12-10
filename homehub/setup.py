61d8256aed10d79bcba54c6acaf0abe8bd9f89bb
bchoward <bchoward@gmail.com>
2015-12-09 19:38:18 -0800

    setup

---
+++ homehub/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'sqlachemy',
+    'python-spi',
+    'picamera',
+    'hcsr04sensor',
+    ]
+
+setup(name='pypinsobj',
+      version='0.1',
+      description='wrapper for RPi',
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='bchoward',
+      author_email='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      """,
+      )
+
