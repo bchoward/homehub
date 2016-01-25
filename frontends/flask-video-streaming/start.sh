0132701e426b0e86827cfe1e720f44bdd87d8e8e
bhoward <bhoward@lexmachina.com>
2016-01-25 11:35:30 -0800

    fixes left over from refactoring

--- frontends/flask-video-streaming/start.sh
+++ frontends/flask-video-streaming/start.sh
@@ -2,13 +2,13 @@
 
 
 HDIR=/home/pi
-PROJRT=homehub/frontends/
+PROJRT=homehub/frontends
 PROJNAME=flask-video-streaming
 EXECNAME=app.py
-VIRTUALENV_DIR=$HDIR/.virtualenvs/$PROJRT
+VIRTUALENV_DIR=$HDIR/.virtualenvs/homehub
 
 
 source $VIRTUALENV_DIR/bin/activate
-cd $HDIR/$PROJRT/$PROJNAME-video-streaming
+cd $HDIR/$PROJRT/$PROJNAME
 
 $VIRTUAL_ENV/bin/python $EXECNAME
